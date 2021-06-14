c52790fc01f5cbe68963155515c220ffa53f1d1f
Eirik <Eirik@DESKTOP-24LKQSM.local>
2021-06-14 20:50:14 +0200

    added figures and adde shapes to data exploration

--- data_exploration.py
+++ data_exploration.py
@@ -20,6 +20,8 @@ test_data = pd.read_csv('test.csv')
 df = pd.DataFrame(data)
 test_df = pd.DataFrame(test_data)
 
+# Shape
+print('Shape of data: {}\n'.format(df.shape))
 # Extract feature and target names
 feature_names = list(df.columns[:-1])
 print('Features:')
@@ -29,23 +31,26 @@ for name in feature_names:
 target_names = df['Loan_Status'].unique()
 print("\nTarget:\nLoan_status: {}".format(target_names))
 
-catergorical = ['Gender', 'Married', 'Dependents', 'Education', 
+cat = ['Gender', 'Married', 'Dependents', 'Education', 
                 'Self_Employed', 'Property_Area','Credit_History',
                 'Loan_Amount_Term']
 
 # Plotting columns vs 'Loan_Status'
-fig, axes = plt.subplots(4,2,figsize=(12,15))
+fig, axes = plt.subplots(4, 2, figsize = (13,13))
 sns.set_theme(style="darkgrid")
-for idx,cat_col in enumerate(catergorical):
-    row,col = idx//2,idx%2
-    sns.countplot(x=cat_col, data=df, ax=axes[row, col], hue='Loan_Status')
+for index, cat_col in enumerate(cat):
+    row, col = index // 2, index % 2
+    sns.countplot(x = cat_col, data = df, ax = axes[row, col], hue='Loan_Status')
     
-numerical = ['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount']
+plt.savefig('Categorical Columns vs Loan Status.png')
 
-fig,axes = plt.subplots(1,3,figsize=(17,5))
-for idx,num_col in enumerate(numerical):
-    sns.boxplot(y=num_col,data=df,x='Loan_Status',ax=axes[idx])
+num = ['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount']
 
+fig,axes = plt.subplots(1, 3, figsize = (13, 13))
+for index , num_col in enumerate(num):
+    sns.boxplot(y = num_col, data = df, x='Loan_Status', ax = axes[index])
+
+plt.savefig('Numerical Columns vs Loan Status.png')
 
 #Target mapping - No: 0, Yes = 1
 target_mapping = {target: idx for idx, target in enumerate(np.unique(df['Loan_Status']))}
@@ -54,7 +59,6 @@ df['Loan_Status'] = df['Loan_Status'].map(target_mapping)
 # Create dummies
 df_encoded = pd.get_dummies(data=df, drop_first=True, columns=['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed','Credit_History', 'Property_Area'])
 test_df_encoded = pd.get_dummies(data=test_df, drop_first=True, columns=['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'Credit_History', 'Property_Area'])
-
 
 #Checking for NaN
 print('NaN in train:\n_______________________\n{}'.format(df_encoded.isnull().sum()))
@@ -66,21 +70,24 @@ descr_stats = df_encoded.describe()
 print('\n{}'.format(descr_stats))
 
 # Creating histograms
-df_encoded.hist(figsize=(14,14))
+df_encoded.hist(figsize = (13, 13))
+plt.savefig('Histograms.png')
 plt.show()
 
 # Creating box plots
-df_encoded.plot(kind='box', 
-                 subplots=True, 
-                 layout=(5,3), 
-                 sharex=False, 
-                 sharey=False,
-       figsize=(14,14))
+df_encoded.plot(kind = 'box', 
+                 subplots = True, 
+                 layout = (5, 3), 
+                 sharex = False, 
+                 sharey = False,
+       figsize=(13, 13))
+
+plt.savefig('Boxplots.png')
 plt.show()
 
 # Creating correlation matrix
 correlation_matrix = df_encoded.corr()
 
-fig, ax = plt.subplots(figsize=(10,10))
+fig, ax = plt.subplots(figsize=(13,10))
 sns.heatmap(correlation_matrix, annot=True, cmap="YlGnBu", ax=ax, vmin=-1, vmax=1)
-
+plt.savefig('Correlation matrix.png')
